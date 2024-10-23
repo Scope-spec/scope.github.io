735d8282507d9b000792c4a7b4f0e770c291dd7a
Scope-spec <davidpriegl@gmail.com>
2024-10-23 22:33:28 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+const apiKey = '19ecb6c8b5429f6be9e713f6ab799710';
+const locButton = document.querySelector('.loc-button');
+const todayInfo = document.querySelector('.today-info');
+const todayWeatherIcon = document.querySelector('.today-weather i');
+const todayTemp = document.querySelector('weather-temp');
